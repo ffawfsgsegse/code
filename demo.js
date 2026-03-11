8db4ce492aa746400328b9853c9531bd7ae00a21
luis suarez <107946905+ffawfsgsegse@users.noreply.github.com>
2026-03-11 16:28:02 +0700

    Delete demo.js

--- demo.js
+++
@@ -1,8 +0,0 @@
- let taikhoan = prompt (` Mời nhập tài khoản`);
-    let matkhau = prompt (` Mời nhập mật khẩu`);
-
-    if ( taikhoan === "admin" && matkhau === "123"){
-        alert (` Đăng nhập thành công`);
-    }else {
-        alert (` Đăng nhập thất bại`);
-    }
